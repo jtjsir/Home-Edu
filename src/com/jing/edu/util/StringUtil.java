f9e4145ec8253d7512fbe4a1c2a938ac77b007a0
jtj <15958042648@163.com>
2016-03-17 17:41:34 +0800

    添加了Netty的服务器处理心跳包类

--- src/com/jing/edu/util/StringUtil.java
+++ src/com/jing/edu/util/StringUtil.java
@@ -1,51 +1,61 @@
 package com.jing.edu.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class StringUtil {
 
 	/**
-	 *前台传送过来的subjects数组转换为String
+	 * 前台传送过来的subjects数组转换为String
+	 * 
 	 * @param subjects
 	 * @return
 	 */
-	public static String subjectsContact(String[] subjects){
-		int subSize = subjects.length ;
-		StringBuffer buffer = new StringBuffer() ;
-		for(int i = 0 ; i < subSize ; i++){
+	public static String subjectsContact(String[] subjects) {
+		int subSize = subjects.length;
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < subSize; i++) {
 			buffer.append(subjects[i]).append(",");
 		}
-		buffer.deleteCharAt(buffer.length()-1) ;
-		
-		return buffer.toString() ;
+		buffer.deleteCharAt(buffer.length() - 1);
+
+		return buffer.toString();
 	}
-	
-	public static String priceContact(String small,String medium,String senior){
-		StringBuffer buffer = new StringBuffer() ;
-		buffer.append("小学报价: ").append(small).append("元/小时;")
-		.append("初中报价: ").append(medium).append("元/小时;").append("高中报价: ")
-		.append(senior).append("元/小时") ;
-		
-		return buffer.toString() ;
+
+	public static String priceContact(String small, String medium, String senior) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("小学报价: ").append(small).append("元/小时;").append("初中报价: ").append(medium).append("元/小时;")
+				.append("高中报价: ").append(senior).append("元/小时");
+
+		return buffer.toString();
 	}
-	
-	public static String[] seperateSubject(String subject){
-		String[] result = new String[3] ;
-		if(subject==null){
-			return null ;
-		}else{
-			int index = subject.indexOf(",") ;
-			if(index==-1){
-				result[0] = subject ;
-				return result ;
+
+	public static String[] seperateSubject(String subject) {
+		String[] result = new String[3];
+		if (subject == null) {
+			return null;
+		} else {
+			int index = subject.indexOf(",");
+			if (index == -1) {
+				result[0] = subject;
+				return result;
 			}
-			int count = 0 ;
-			while(index!=-1){
-				result[count] = subject.substring(0, index) ;
-				subject = subject.substring(index+1) ;
-				index = subject.indexOf(",") ;
-				count++ ;
+			int count = 0;
+			while (index != -1) {
+				result[count] = subject.substring(0, index);
+				subject = subject.substring(index + 1);
+				index = subject.indexOf(",");
+				count++;
 			}
-			result[count] = subject ;
+			result[count] = subject;
 		}
-		return result ;
+		return result;
+	}
+
+	public static String getNowFormatTime() {
+		Calendar calendar = Calendar.getInstance();
+		java.util.Date date = calendar.getTime();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return formatter.format(date);
 	}
 }
